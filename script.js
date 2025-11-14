f5b92a4be9fcb18518192d633823c2ec0a9804f1
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-11-14 12:18:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+let student = {
+	name: 'Mahendra',
+	age: 21,
+	city: 'kakinada'
+};
+
+function getKeys(obj){
+	return Object.keys(obj);
+	
+}
+
+console.log(getKeys(student));
